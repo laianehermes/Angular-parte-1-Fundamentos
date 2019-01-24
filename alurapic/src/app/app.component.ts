6894000817a8b10aa2434b2e063c75e5dbb549a5
laianehermes <laiane.sp@hotmail.com>
2019-01-23 23:20:26 -0200

    Parte de rotas do angular
        BrowserModule vs CommonModule
        Single Page Application e rotas no lado do navegador
        O módulo RouterModule
        A diretiva router-outlet como grande lacuna para exibição de outros componentes
        Módulo de rotas e definição de rotas
        Como lidar com páginas 404
        Parametrizando rotas e como obter valores do segmento parametrizado

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [];
-
-  constructor(http: HttpClient){
-
-  }
+export class AppComponent{
+  
 }
--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
